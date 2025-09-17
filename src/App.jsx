444900b12268320318b0a0969d9bf1e369511c81
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-09-17 17:53:13 +0530

    Added routes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+export default function App() {
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <h1>hHomePage</h1>
+    </div>
+  );
+}
