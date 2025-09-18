01e5f141a9b437865522dc20db0e0b299822c26f
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-09-18 22:16:13 +0530

    Styling SideBarr

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import assets, { userDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 const SideBar = ({ selectedUser, setSelectedUser }) => {
